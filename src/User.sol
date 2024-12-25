60900c3ce42b1119539cdc5236f5973d771b7d91
Lr <1216052505@qq.com>
2024-12-25 16:59:55 +0800

    n

--- src/User.sol
+++ src/User.sol
@@ -1,24 +1,24 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import "../src/movie.sol";
+// import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+// import "../src/movie.sol";
 
-contract User{
+// contract User{
 
-    using EnumerableSet for EnumerableSet.UintSet;
+//     using EnumerableSet for EnumerableSet.UintSet;
 
-    // movie_id -> mapping<address,score>
-    mapping(string=>mapping(address=>uint8)) userScores ;
+//     // movie_id -> mapping<address,score>
+//     mapping(string=>mapping(address=>uint8)) userScores ;
 
-    mapping(string => uint8) scores;
+//     mapping(string => uint8) scores;
 
     
-    //打分 1~5
-    function socre(string memory movieId , uint8 score) public {
+//     //打分 1~5
+//     function socre(string memory movieId , uint8 score) public {
 
-    }
+//     }
 
 
-    //购买nft
-}
+//     //购买nft
+// }
