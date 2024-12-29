91935672d2ef65988e2dab0d09a904476029a127
Lr <1216052505@qq.com>
2024-12-29 14:57:21 +0800

    提交

--- src/User.sol
+++ src/User.sol
@@ -1,24 +1,26 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-// import "../src/movie.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "../src/movie.sol";
 
-// contract User{
+contract User{
 
-//     using EnumerableSet for EnumerableSet.UintSet;
+    using EnumerableSet for EnumerableSet.UintSet;
 
-//     // movie_id -> mapping<address,score>
-//     mapping(string=>mapping(address=>uint8)) userScores ;
+    // 我的电影评分
+    mapping(address=>mapping(uint256=>uint8)) userScores ;
 
-//     mapping(string => uint8) scores;
-
+    //记录电影评分日期 用户地址=>(日期=>电影Id)
+    mapping(address=>mapping(uint256=>uint256)) userScoreCount;
     
-//     //打分 1~5
-//     function socre(string memory movieId , uint8 score) public {
+    
+    //打分 1~5
+    function socre(uint256 movieId , uint8 score) public {
+        userScores[msg.sender][movieId] = score ;
+    }
 
-//     }
 
-
-//     //购买nft
-// }
+    //购买nft
+    //校验今日评分次数是否大于5次，如果大于5次则报错 单日评价次数不得大于5次；
+}
--- test/Movie.t.sol
+++ test/Movie.t.sol
@@ -1,40 +1,40 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.20;
-import "forge-std/Test.sol";
-import "forge-std/console.sol";
-import "../src/Movie.sol";
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity 0.8.20;
+// import "forge-std/Test.sol";
+// import "forge-std/console.sol";
+// import "../src/Movie.sol";
 
-contract MovieTest is Test {
-    Movie private nft;
+// contract MovieTest is Test {
+//     Movie private nft;
 
-    function setUp() public {
-        nft = new Movie();
-    }
+//     function setUp() public {
+//         nft = new Movie();
+//     }
 
-    function testMintNFT() public {
-        string[] memory picArray = new string[](2);
-        picArray[0] = "xxxxx0000000001";
-        picArray[1] = "xxxxx0000000002";
+//     function testMintNFT() public {
+//         string[] memory picArray = new string[](2);
+//         picArray[0] = "xxxxx0000000001";
+//         picArray[1] = "xxxxx0000000002";
 
-        nft.mint(
-            msg.sender,
-            unicode"蜘蛛侠1",
-            unicode"2000-01-01",         
-            picArray
-        );
+//         nft.mint(
+//             msg.sender,
+//             unicode"蜘蛛侠1",
+//             unicode"2000-01-01",         
+//             picArray
+//         );
 
-        (string memory name, string memory date, string[] memory pics) = nft
-            .getMovie(1);
-        console.log("------------------++++++");
-        console.log("Movie Name:", name);
-        console.log("Release Date:", date);
-        for (uint256 i = 0; i < pics.length; i++) {
-            console.log(pics[i]);
-        }
-    }
+//         (string memory name, string memory date, string[] memory pics) = nft
+//             .getMovie(1);
+//         console.log("------------------++++++");
+//         console.log("Movie Name:", name);
+//         console.log("Release Date:", date);
+//         for (uint256 i = 0; i < pics.length; i++) {
+//             console.log(pics[i]);
+//         }
+//     }
 
-    // function testConsole() public pure {
-    //     console.log("----sdsadasd-----++++++");
-    //     console.log("------------------++++++");
-    // }
-}
+//     // function testConsole() public pure {
+//     //     console.log("----sdsadasd-----++++++");
+//     //     console.log("------------------++++++");
+//     // }
+// }
